9e253610838932e4764f32680c1a04e1ee401cea
andela-gnyenyeshi <gertrude.nyenyeshi@andela.com>
2019-05-21 15:21:19 +0300

    server setup

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log('HELLO');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const DIST_DIR = path.join(__dirname, 'dist');
+const HTML_FILE = path.join(DIST_DIR, 'index.html');
+
+app.use(express.static(DIST_DIR));
+
+app.get('/', (req, res) => {
+    res.sendFile(HTML_FILE);
+});
+
+app.listen(3030, () => {
+    console.log('Live on port 3030');
+});
